181adc44bb9ae10a61ad3273142dfcbe9ae82bca
Lora <lorageo33@gmail.com>
2025-12-07 18:31:03 +0200

    Initial commit

--- src/lesson_18/homework/Task1.java
+++ src/lesson_18/homework/Task1.java
@@ -11,23 +11,17 @@ public class Task1 {
          */
         Scanner input = new Scanner(System.in);
 
-        int result = 0;
+        int result = 1;
 
         while (true) {
-            System.out.println("Please enter a first number: ");
-            int firstNumber = Integer.parseInt(input.nextLine());
-            if (firstNumber == 0) {
+            System.out.println("Enter number:");
+            int number = Integer.parseInt(input.nextLine());
+
+            if (number == 0) {
                 System.out.println("Result: " + result);
                 break;
-            }
-            System.out.println("Please enter a second number: ");
-            int secondNumber = Integer.parseInt(input.nextLine());
-            if (secondNumber == 0) {
-                System.out.println("Result: " + result);
-                break;
-            }
-            if (firstNumber % 2 == 0 && secondNumber % 2 == 0) {
-                result = firstNumber * secondNumber;
+            } else if (number % 2 == 0) {
+                result *= number;
             }
         }
     }
