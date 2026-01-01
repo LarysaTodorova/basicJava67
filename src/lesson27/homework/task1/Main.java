0a010554c28f59c2d560faa5ae30f8704ed8ce9b
Lora <lorageo33@gmail.com>
2026-01-01 22:10:49 +0200

    Initial commit

--- src/lesson27/homework/task1/Main.java
+++ src/lesson27/homework/task1/Main.java
@@ -1,5 +1,8 @@
 package lesson27.homework.task1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         /*
@@ -9,5 +12,31 @@ public class Main {
         Выведите оба списка в консоль, проконтролируйте результат.
          */
 
+        List<String> fruits = new ArrayList<>();
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Orange");
+        fruits.add("Watermelon");
+        fruits.add("Pear");
+        fruits.add("Strawberry");
+        fruits.add("Mango");
+        fruits.add("Pineapple");
+
+        List<String> fruits2 = new ArrayList<>();
+
+//        for (int i = 0; i < fruits.size(); i++) {
+//            if (fruits.get(i).length() % 2 != 0) {
+//                fruits2.add(fruits.get(i));
+//            }
+//        }
+
+        for (String fruit : fruits) {
+            if (fruit.length() % 2 != 0) {
+                fruits2.add(fruit);
+            }
+        }
+
+        System.out.println(fruits);
+        System.out.println(fruits2);
     }
 }
--- src/lesson27/homework/task2/Main.java
+++ src/lesson27/homework/task2/Main.java
@@ -1,5 +1,8 @@
 package lesson27.homework.task2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         /*
@@ -8,5 +11,22 @@ public class Main {
         Выведите в консоль список и значение переменной, проконтролируйте результат.
          */
 
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(21);
+        numbers.add(3);
+        numbers.add(1);
+        numbers.add(4);
+        numbers.add(7);
+        numbers.add(50);
+        numbers.add(15);
+
+        int sum = 0;
+
+        for (Integer number : numbers) {
+            sum += number;
+        }
+
+        System.out.println(numbers);
+        System.out.println(sum);
     }
 }
