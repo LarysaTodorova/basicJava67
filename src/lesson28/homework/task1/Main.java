4ca3059f836139ac0e16a7cddb713a5fded5f5c9
Lora <lorageo33@gmail.com>
2026-01-03 15:45:03 +0200

    Initial commit

--- src/lesson28/homework/task1/Main.java
+++ src/lesson28/homework/task1/Main.java
@@ -1,11 +1,33 @@
 package lesson28.homework.task1;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         /*
         Создайте список чисел - 4, 8, 7, 10, 5, 2, 9, 11, 5, 8, 0, 5. Выведите его в консоль.
         Удалите из списка все числа, которые делятся нацело на 5. Снова выведите список в консоль.
-
          */
+
+        List<Integer> numbers = new ArrayList<>(List.of(-4, 8, 7, 10, 5, 2, 9, 11, 5, 8, 0, 5));
+
+        System.out.println("List before delete:");
+        System.out.println(numbers);
+
+        System.out.println();
+
+        Iterator<Integer> iterator = numbers.iterator();
+
+        while (iterator.hasNext()) {
+            Integer number = iterator.next();
+            if (number % 5 == 0) {
+                iterator.remove();
+            }
+        }
+
+        System.out.println("List after delete:");
+        System.out.println(numbers);
     }
 }
--- src/lesson28/homework/task2/Main.java
+++ src/lesson28/homework/task2/Main.java
@@ -1,13 +1,54 @@
 package lesson28.homework.task2;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
 public class Main {
     public static void main(String[] args) {
-
         /*
         Создайте список чисел - 4, 8, 7, 10, 5, 2, 9, 11, 5, 8, 0, 5. Выведите его в консоль.
         Двигаясь по списку, найдите число, которое больше 10.
         Удалите из списка все чётные числа перед этим числом. Снова выведите список в консоль.
         Если такое число не было найдено, то и удалять ничего не нужно.
          */
+
+        List<Integer> numbers = new ArrayList<>(List.of(-4, 8, 7, 10, 5, 2, 9, 11, 5, 8, 0, 5));
+
+        System.out.println("List before delete:");
+        System.out.println(numbers);
+
+        System.out.println();
+
+        ListIterator<Integer> iterator = numbers.listIterator();
+
+        while (iterator.hasNext()) {
+            Integer number = iterator.next();
+            if (number > 10) {
+                iterator.previous();
+
+                while (iterator.hasPrevious()) {
+                    number = iterator.previous();
+                    if (number % 2 == 0) {
+                        iterator.remove();
+                    }
+                }
+                break;
+            }
+        }
+
+//        while (iterator.hasNext()) {
+//            Integer number = iterator.next();
+//            if (number > 10) {
+//                break;
+//            }
+//            if (number % 2 == 0) {
+//                iterator.remove();
+//            }
+//        }
+
+        System.out.println("List after delete:");
+        System.out.println(numbers);
+
     }
 }
