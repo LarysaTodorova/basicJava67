ad2c9bcd33da49ce06c6a36edd3e9f2a814144dd
Lora <lorageo33@gmail.com>
2026-01-15 20:50:27 +0200

    initial commit

--- src/lesson31Map/jptPactice/task4/Main.java
+++ src/lesson31Map/jptPactice/task4/Main.java
@@ -1,5 +1,8 @@
 package lesson31Map.jptPactice.task4;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
         /*
@@ -11,5 +14,23 @@ public class Main {
 Вывести его название и цену
 Подсказка: сравнивать значения внутри цикла
          */
+
+        Map<String, Double> products = new HashMap<>();
+        products.put("Apple", 4.0);
+        products.put("Orange", 6.0);
+        products.put("Lemon", 7.0);
+        products.put("Banana", 4.50);
+        products.put("Kiwi", 7.50);
+        products.put("Pineapple", 11.50);
+
+        Double maxPrice = 0.0;
+
+        for (Map.Entry<String, Double> product : products.entrySet()) {
+            if (product.getValue() > maxPrice) {
+                maxPrice = product.getValue();
+            }
+        }
+
+        System.out.println("Max price: " + maxPrice);
     }
 }
--- src/lesson31Map/jptPactice/task5/Main.java
+++ src/lesson31Map/jptPactice/task5/Main.java
@@ -1,5 +1,10 @@
 package lesson31Map.jptPactice.task5;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
         /*
@@ -10,5 +15,23 @@ List<String> words = List.of("Apple", "Avocado", "Banana", "Blueberry", "Cherry"
 Нужно получить:
 Map<Character, List<String>>
          */
+
+        List<String> words = List.of("Apple", "Avocado", "Banana", "Blueberry", "Cherry");
+
+        Map<Character, List<String>> myMap = new HashMap<>();
+
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                word = word.toLowerCase();
+                Character letter = word.charAt(0);
+                if (!myMap.containsKey(letter)) {
+                    myMap.put(letter, new ArrayList<>());
+                }
+                myMap.get(letter).add(word);
+            }
+        }
+
+        System.out.println(myMap);
+
     }
 }
