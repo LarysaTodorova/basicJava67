545c6f053b3a1f021f154e7c1dd80a15cc524b5f
Lora <lorageo33@gmail.com>
2026-02-15 23:28:30 +0200

    initial commit

--- src/lesson38InputOutput/homework/task2/Main.java
+++ src/lesson38InputOutput/homework/task2/Main.java
@@ -1,14 +1,15 @@
 package lesson38InputOutput.homework.task2;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class Main {
     public static void main(String[] args) {
         /*
         Создайте в корне проекта файл cat.txt
-Поместите в файл вручную следующую информацию - Hello! I am cat! My name is Jasper.
-I like to eat and my human named Eva.
+Поместите в файл вручную следующую информацию - Hello! I am cat! My name is Jasper. I like to eat and my human named Eva.
 Создайте поток ввода из этого файла.
 Пропустите 36 байт информации из файла (метод skip).
 Следующие 7 байт преобразуйте в символы и выведите в консоль в одну строку.
@@ -21,9 +22,27 @@ I like to eat and my human named Eva.
         try {
             file.createNewFile();
 
+            InputStream firstIn = new FileInputStream(file);
+            firstIn.skip(36);
+            byte[] firstPart = new byte[7];
+            firstIn.read(firstPart);
+            String firstText = new String(firstPart);
+            System.out.print(firstText);
+
+            InputStream secondIn = new FileInputStream(file);
+            secondIn.skip(28);
+            byte[] secondPart = new byte[2];
+            secondIn.read(secondPart);
+            System.out.print(new String(secondPart));
+
+            secondIn.skip(40);
+            byte[] thirdPart = new byte[2];
+            secondIn.read(thirdPart);
+            System.out.print(new String(thirdPart));
+
 
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.out.println(e.getMessage());
         }
     }
 }
